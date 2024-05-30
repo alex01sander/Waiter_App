ccc91986fcd06bc46592431fe63542c8e16b1a8f
Alex Sander <alexsander01@hotmail.com.br>
2024-05-30 00:37:57 -0300

    chore: Improved TypeScript configuration and ensured explicit typing in Text component

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Text } from './src/components/Text';
 
 export default function App() {
+
+    const [isFontLoaded] = useFonts({
+        'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+        'GeneralSans-500': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+        'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Bold.otf')
+    })
+
+    if(!isFontLoaded){
+        return null;
+    }
+
   return (
     <View style={styles.container}>
-      <Text>teste</Text>
+      <Text size={24} weight={600}>teste</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#8b0707',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
