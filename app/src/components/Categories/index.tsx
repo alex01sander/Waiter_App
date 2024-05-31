f2a547bb2ea617a074f335c26dea9901bb21c60e
Alex Sander <alexsander01@hotmail.com.br>
2024-05-31 16:21:13 -0300

    feat: Create category list components and styles

--- app/src/components/Categories/index.tsx
+++ app/src/components/Categories/index.tsx
@@ -1,12 +1,44 @@
-import { categories } from '../../mocks/categories'
-import { Category } from './styles'
+import { categories } from '../../mocks/categories';
+import { Category, Icon } from './styles';
+import { Text } from "../../components/Text";
+import { FlatList } from 'react-native';
+import { useState } from 'react';
+
+
 
 export function Categories() {
-    return (
-        categories.map((category) => {
-            <Category key={category._id}>
 
-            </Category>
-        })
-    )
+    const [selectedCategory, setSelectedCategory] = useState('')
+
+    function handleSelectCategory(categoryId: string){
+        const category = selectedCategory === categoryId ? '' : categoryId;
+        setSelectedCategory(category)
+    }
+
+    return (
+
+        <FlatList
+                horizontal
+                data={categories}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{paddingRight: 24}}
+                keyExtractor={category => category._id}
+                renderItem={({ item: category}) => {
+                    const isSelected = selectedCategory === category._id
+
+                    return(
+                    <Category onPress={() =>handleSelectCategory(category._id)}>
+                        <Icon>
+                            <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
+                        </Icon>
+                        <Text size={14} weight='700' opacity={isSelected ? 1 : 0.5}>{category.name}</Text>
+                    </Category>
+
+                    )
+                }}
+        />
+
+
+
+    );
 }
--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export function Header(){
     return(
         <Container>
             <Text size={14} opacity={0.9}>Bem-vindo(a) ao</Text>
-            <Text size={24} wight='700'>
+            <Text size={24} weight='700'>
                 WAITER
                 <Text size={24}>APP</Text>
             </Text>
