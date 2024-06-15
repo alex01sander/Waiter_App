64cad7a14089fa729d8c07891563dcfb62b475cf
Alex Sander <alexsander01@hotmail.com.br>
2024-06-15 11:50:51 -0300

    refactor: Changed font to bold

--- app/App.tsx
+++ app/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
 
     const [isFontLoaded] = useFonts({
         'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
-        'GeneralSans-500': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+        'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
         'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf')
     })
 
